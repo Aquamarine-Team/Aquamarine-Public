565714a3a5ad27f484d8e93f908202e78bb82319
MaruselPlay <62075300+MaruselPlay@users.noreply.github.com>
2022-08-28 18:54:06 +0000

    start.cmd added

---
+++ start.cmd
@@ -0,0 +1,39 @@
+@echo off
+TITLE Aquamarine
+
+:start
+if exist bin\php\php.exe (
+	set PHPRC=""
+	set PHP_BINARY=bin\php\php.exe
+) else (
+	set PHP_BINARY=php
+)
+
+if exist LiteCore*.phar (
+	set POCKETMINE_FILE=LiteCore*.phar
+) else (
+	if exist LiteCore.phar (
+		set POCKETMINE_FILE=LiteCore.phar
+	) else (
+		if exist PocketMine-MP.phar (
+			set POCKETMINE_FILE=PocketMine-MP.phar
+		) else (
+		    if exist src\pocketmine\PocketMine.php (
+		        set POCKETMINE_FILE=src\pocketmine\PocketMine.php
+			) else (
+		        echo "Не удалось найти правильную установку Aquamarine."
+		        pause
+		        exit 1
+		    )
+	    )
+	)
+)
+
+if exist bin\mintty.exe (
+	start "" bin\mintty.exe -o Columns=88 -o Rows=32 -o AllowBlinking=0 -o FontQuality=3 -o Font="Consolas" -o FontHeight=10 -o CursorType=0 -o CursorBlinks=1 -h error -t " Aquamarine" -w max %PHP_BINARY% %POCKETMINE_FILE% --enable-ansi %*
+) else (
+	%PHP_BINARY% -c bin\php %POCKETMINE_FILE% %*
+)
+timeout 10
+goto start
+pause
